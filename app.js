fdd7fa4f51b53db9318e65e072825f4a4d9fa716
pasisaa <kiikasimm31@gmail.com>
2024-02-07 18:58:00 +0800

    kasimaja

---
+++ app.js
@@ -0,0 +1,58 @@
+$(document).ready(() => {
+    $('#hamburger-menu').click(() => {
+        $('#hamburger-menu').toggleClass('active')
+        $('#nav-menu').toggleClass('active')
+    })
+
+    // setting owl carousel
+
+    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+
+    $('#hero-carousel').owlCarousel({
+        items: 1,
+        dots: false,
+        loop: true,
+        nav:true,
+        navText: navText,
+        autoplay: true,
+        autoplayHoverPause: true
+    })
+
+    $('#top-movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            300: {
+                items: 3
+            },
+            1080: {
+                items: 4
+            },
+            1400: {
+                items: 6
+            }
+        }
+    })
+
+    $('.movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        nav:true,
+        navText: navText,
+        margin: 15,
+        responsive: {
+            300: {
+                items: 2
+            },
+            1080: {
+                items: 4
+            },
+            1400: {
+                items: 6
+            }
+        }
+    })
+})
